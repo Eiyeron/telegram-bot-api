c1596b20f215bd52505d165c4e4ef3408a7e9ef0
Eiyeron Fulmicendii <eiyeron@gmail.com>
2015-06-27 22:42:30 +0200

    PEP8 compliant, checked with pip's pep8 tool.

--- models.py
+++ models.py
@@ -2,13 +2,15 @@ class User:
     def __init__(self, data):
         self.id = data["id"]
         self.first_name = data["first_name"]
-        self.last_name = data.get("last_name","")
-        self.username = data.get("username","")
+        self.last_name = data.get("last_name", "")
+        self.username = data.get("username", "")
+
 
 class GroupChat:
     def __init__(self, data):
         self.id = data["id"]
         self.title = data["title"]
+
 
 class Message:
     def __init__(self, data):
@@ -23,61 +25,77 @@ class Message:
             self.chat = User(data["chat"])
 
         # Forward data
-        # Idea : as date is there if there is from, move date into from's condition?
+        # Idea : as date is there if there is from,
+        # move date into from's condition?
         if "forward_from" in data:
             self.forward_from = User(data["forward_from"])
+
         if "forward_date" in data:
             self.forward_date = data["forward_date"]
 
-        #Message types
+        # Message types
         if "reply_to_message" in data:
             self.reply_to_message = Message(data["reply_to_message"])
+
         if "text" in data:
             self.text = data["text"]
+
         if "audio" in data:
             self.audio = Audio(data["audio"])
+
         if "document" in data:
             self.document = Document(data["document"])
+
         if "photo" in data:
             self.photo = []
             for photo in data["photo"]:
                 self.photo.append(PhotoSize(photo))
+
         if "sticker" in data:
             self.sticker = Sticker(data["sticker"])
+
         if "video" in data:
             self.video = Video(data["video"])
+
         if "contact" in data:
             self.contact = Contact(data["contact"])
+
         if "location" in data:
             self.location = Location(data["location"])
 
         # What happened in the server
         if "new_chat_participant" in data:
             self.new_chat_participant = User(data["new_chat_participant"])
+
         if "left_chat_participant" in data:
             self.left_chat_participant = User(data["left_chat_participant"])
+
         if "new_chat_title" in data:
             self.new_chat_title = data["new_chat_title"]
+
         if "new_chat_photo" in data:
             self.new_chat_photo = []
             for photo in data["new_chat_photo"]:
                 self.new_chat_photo.append(PhotoSize(photo))
+
         if "delete_chat_photo" in data:
             self.delete_chat_photo = data["delete_chat_photo"]
+
         if "group_chat_created" in data:
             self.group_chat_created = data["group_chat_created"]
 
 
-
-# Todo? : Inheritance and create a File superclass for all file-related classes?
-
+# Todo? : Inheritance and create a File superclass
+# for all file-related classes?
 class PhotoSize:
     def __init__(self, data):
-        if not data: return
+        if not data:
+            return
         self.file_id = data["file_id"]
         self.width = data["width"]
         self.height = data["height"]
         self.file_size = data.get("file_size", -1)
+
 
 class Audio:
     def __init__(self, data):
@@ -85,6 +103,7 @@ class Audio:
         self.duration = data["duration"]
         self.mime_type = data["mime_type"]
         self.file_size = data.get("file_size", -1)
+
 
 class Document:
     def __init__(self, data):
@@ -94,6 +113,7 @@ class Document:
         self.mime_type = data.get("mime_type", "")
         self.file_size = data.get("file_size", -1)
 
+
 class Sticker:
     def __init__(self, data):
         self.file_id = data["file_id"]
@@ -101,6 +121,7 @@ class Sticker:
         self.height = data["height"]
         self.thumb = PhotoSize(data["thumb"])
         self.file_size = data.get("file_size", -1)
+
 
 class Video:
     def __init__(self, data):
@@ -113,6 +134,7 @@ class Video:
         self.file_size = data.get("file_size", -1)
         self.caption = data.get("caption", "")
 
+
 class Contact:
     def __init__(self, data):
         self.phone_number = data["phone_number"]
@@ -120,10 +142,12 @@ class Contact:
         self.last_name = data.get("last_name", "")
         self.user_id = data.get("user_id", "")
 
+
 class Location:
     def __init__(self, data):
         self.longitude = data["longitude"]
         self.latitude = data["latitude"]
+
 
 class UserProfilePhotos:
     def __init__(self, data):
@@ -132,19 +156,19 @@ class UserProfilePhotos:
         for row in data["photos"]:
             self.photos.append(list(row))
 
-# Todo? Keyboard superclass for inheritance
 
+# Todo? Keyboard superclass for inheritance
 class ReplyKeybaordMarkup:
     def __init__(self, data):
         self.keyboard = []
         for row in data["keyboard"]:
             self.keyboard.append(list(row))
-        self.reisze_keyboard = data.get("resize_keyboard", False)
-        self.one_time_keyboard = data.get("one_time_keyboard", False)
-        self.selective = data.get("selective", False)
+            self.reisze_keyboard = data.get("resize_keyboard", False)
+            self.one_time_keyboard = data.get("one_time_keyboard", False)
+            self.selective = data.get("selective", False)
+
 
 class ReplyKeyboardHide:
     def __init__(self, data):
         self.force_reply = data["force_reply"]
         self.selective = data.get("selective", False)
-
--- telegram.py
+++ telegram.py
@@ -1,27 +1,30 @@
 import requests
 from .models import Message
 
+
 class Telegram:
-    # TODO ? : Convert this into a simple array and get value by doing "on_"+"value"
+    # TODO ? : Convert this into a simple array
+    # and get value by doing "on_"+"value"
     handlerTypeCallback = {
-        "update":"on_update",
-        "forward_from":"on_forward",
-        "reply_to_message":"on_reply",
-        "text":"on_text",
-        "audio":"on_audio",
-        "document":"on_document",
-        "photo":"on_photo",
-        "sticker":"on_sticker",
-        "video":"on_video",
-        "contact":"on_contact",
-        "location":"on_location",
-        "new_chat_participant":"on_new_chat_carticipant",
-        "left_chat_participant":"on_left_chat_participant",
-        "new_chat_title":"on_new_chat_title",
-        "new_chat_photo":"on_new_chat_photo",
-        "delete_chat_Photo":"on_delete_chat_photo",
-        "group_chat_created":"on_group_chat_created",
+        "update": "on_update",
+        "forward_from": "on_forward",
+        "reply_to_message": "on_reply",
+        "text": "on_text",
+        "audio": "on_audio",
+        "document": "on_document",
+        "photo": "on_photo",
+        "sticker": "on_sticker",
+        "video": "on_video",
+        "contact": "on_contact",
+        "location": "on_location",
+        "new_chat_participant": "on_new_chat_carticipant",
+        "left_chat_participant": "on_left_chat_participant",
+        "new_chat_title": "on_new_chat_title",
+        "new_chat_photo": "on_new_chat_photo",
+        "delete_chat_Photo": "on_delete_chat_photo",
+        "group_chat_created": "on_group_chat_created",
     }
+
     def __init__(self, api_url, token):
         self.api_url = api_url
         self.access_token = token
@@ -30,42 +33,50 @@ class Telegram:
         self.handlers = []
 
     def send_request(self, action, params):
-        url = "{}{}/{}".format(self.api_url,self.access_token,action)
+        url = "{}{}/{}".format(self.api_url, self.access_token, action)
         r = requests.get(url, params=params)
         return r.json()
 
     def get_updates(self, offset=0, limit=100, timeout=0):
-        return self.send_request("getUpdates", {"offset":offset, "limit":limit, "timeout":timeout})
+        return self.send_request("getUpdates", {"offset": offset,
+                                                "limit": limit,
+                                                "timeout": timeout})
 
-    def send_message(self, chat_id, text, reply_to_message=None, reply_markup=None):
-        params = {"chat_id":chat_id, "text":text}
+    def send_message(self, chat_id, text,
+                     reply_to_message=None,
+                     reply_markup=None):
+        params = {"chat_id": chat_id, "text": text}
         if reply_to_message is not None:
             params["reply_to_message"] = reply_to_message
-        if reply_markup is not None:
-            params["reply_markup"] = reply_markup
-        return self.send_request("sendMessage", params)
+            if reply_markup is not None:
+                params["reply_markup"] = reply_markup
+                return self.send_request("sendMessage", params)
 
     def forward_message(self, chat_id, from_chat_id, message_id):
-        return self.send_request("forwardMessage", {"chat_id":chat_id, "from_chat_id":from_chat_id, "message_id": message_id})
+        return self.send_request("forwardMessage",
+                                 {"chat_id": chat_id,
+                                  "from_chat_id": from_chat_id,
+                                  "message_id": message_id})
 
     def add_handler(self, handler):
-        if not "callback" in self.handlers:
+        if "callback" not in self.handlers:
             self.handlers.append(handler)
-    
+
     def remove_handler(self, callback, **kwargs):
         if callback in self.handlers:
             self.handlers.remove(callback)
-    
+
     def call_handlers(self, message):
         for handler in self.handlers:
-            for k,v in self.handlerTypeCallback.items():
-                if (k=="update" or hasattr(message, k)) and hasattr(handler, v):
+            for k, v in self.handlerTypeCallback.items():
+                if (k == "update" or hasattr(message, k))\
+                   and hasattr(handler, v):
                     try:
                         getattr(handler, v)(self, message)
                     except:
-                        print("Oops, there has been a problem with this handler : {}".format(handler))
+                        print("""Oops, there has been a problem
+                        with this handler : {}""".format(handler))
                         print(sys.exc_info()[0])
-
 
     def process_updates(self):
         loopingUpdateHandler = True
@@ -78,4 +89,3 @@ class Telegram:
                     self.call_handlers(message)
             else:
                 print("Oops, something went bad : {}".format(notifications))
-
